6d249708f454eef7638d2f88fae483970eb36e5d
Bright Atsighi <brytebee@gmail.com>
2025-08-15 02:46:21 +0100

    File re-structured

--- alx_backend_graphql_crm/urls.py
+++ alx_backend_graphql_crm/urls.py
@@ -1,5 +1,14 @@
 """
-This folder and its content has been moved to graphql_crm/
+URL configuration for graphql_crm project.
 
-alx_backend_graphql_crm/urls.py
+graphql_crm/urls.py
 """
+from django.contrib import admin
+from django.urls import path
+from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
+
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True)))
+]
